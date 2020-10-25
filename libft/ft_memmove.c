395c768b012f17a6f8fc46024bd14dd890e3af78
roms <romain.berthaud812@gmail.com>
2020-10-25 22:01:33 +0100

    fix memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: roms <romain.berthaud812@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 20:56:49 by roms              #+#    #+#             */
-/*   Updated: 2020/10/25 21:27:36 by roms             ###   ########.fr       */
+/*   Updated: 2020/10/25 22:01:13 by roms             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,26 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*temp;
 	size_t i;
 
 	i = 0;
-	temp = malloc(sizeof(*temp) * n);
-	if (!temp)
+	if (!dest || !src)
 		return (NULL);
-	while (i < n)
-		temp[i] = *(char*)(src + i++);
-	i = 0;
-	while (i < n)
-		*(char*)(dest + i) = temp[i++];
-	i = 0;
-	while (i < n)
-		free(temp[i++]);
+	if (dest > src)
+	{
+		while (len > i)
+		{
+			*(char*)(dest + n - 1) = *(char*)(src + n - 1);
+			n--;
+		}
+	}
+	else
+	{
+		while (i < len)
+		{
+			*(char*)(dest + i) = *(char*)(src + i);
+			i++;
+		}
+	}
 	return (dest);
 }
