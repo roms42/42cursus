1654a25c4c3f5245dd97f5acd4c824b11da5351b
Romain Berthaud <romainberthaud@romains-macbook-pro.home>
2020-11-05 17:45:02 +0100

    addback fix

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: rberthau <rberthau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 15:53:02 by rberthau          #+#    #+#             */
-/*   Updated: 2020/11/05 17:11:14 by rberthau         ###   ########.fr       */
+/*   Updated: 2020/11/05 17:44:55 by rberthau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@ void	ft_lstadd_back(t_list **alst, t_list *new)
 	t_list *tmp;
 
 	tmp = *alst;
+	if (!new)
+		return;
 	new->next = NULL;
 	if (alst == NULL)
 		return;
 	if (*alst == NULL)
+	{
 		*alst = new;
+		return;
+	}
 	else
 	{
-		while (tmp != NULL)
-			tmp = tmp->next;
+		tmp = ft_lstlast(*alst);;
 		tmp->next = new;
 	}
 }
