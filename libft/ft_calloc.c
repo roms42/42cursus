7650c0b8cad416e9b84f2b92efa51b628e0b2986
Romain Berthaud <romainberthaud@romains-macbook-pro.home>
2020-11-05 18:04:25 +0100

    calloc fix

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: roms <romain.berthaud812@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 20:30:57 by roms              #+#    #+#             */
-/*   Updated: 2020/11/05 17:52:16 by rberthau         ###   ########.fr       */
+/*   Updated: 2020/11/05 18:04:17 by rberthau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,15 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	size_t i;
-	void **tab;
+	void *tab;
 
-	i = 0;
+	if (size == 0)
+		size = 1;
 	if (!nmemb)
 		nmemb = 1;
-	tab = malloc(sizeof(*tab) * nmemb);
+	tab = malloc(sizeof(*tab) * (nmemb * size);
 	if (!tab)
 		return (NULL);
-	while (i < nmemb)
-	{
-		tab[i] = malloc(sizeof(**tab) * size);
-		if (!tab[i])
-			return (NULL);
-		ft_bzero(tab[i], size);
-		i++;
-	}
-	return (tab[i - i]);
+	ft_bzero(tab, (nmemb * size);
+	return (tab);
 }
