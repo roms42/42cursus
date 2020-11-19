70aeadce4dffe068e4fa89652785083c05776c8d
Romain Berthaud <romainberthaud@romains-macbook-pro.home>
2020-11-19 18:24:06 +0100

    strtrim fixed

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: roms <romain.berthaud812@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/23 17:36:56 by roms              #+#    #+#             */
-/*   Updated: 2020/11/16 15:31:54 by rberthau         ###   ########.fr       */
+/*   Updated: 2020/11/19 18:23:42 by rberthau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,13 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	dst = ft_strchr(set, s1[i]);
 	while (s1[i] && dst != NULL)
-	{
-		i++;
-		dst = ft_strchr(set, s1[i]);
-	}
+		dst = ft_strchr(set, s1[++i]);
+	if (!s1[i])
+		return (dst);
 	dstlen = ft_strlen(s1 + i);
 	dst = ft_strchr(set, s1[i + dstlen - 1]);
 	while (dst != NULL)
-	{
-		dstlen--;
-		dst = ft_strchr(set, s1[i + dstlen - 1]);
-	}
+		dst = ft_strchr(set, s1[i + (--dstlen) - 1]);
 	dst = ft_substr(s1, i, dstlen);
 	return (dst);
 }
