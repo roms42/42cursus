44649e4d6a2ef4f3f9edefdab49373222c96eeb9
Romain BERTHAUD <roms@Romains-MacBook-Pro.local>
2021-03-17 20:29:40 +0100

    lestclear change order of lst and pointer in protection

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: rberthau <rberthau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 15:15:53 by rberthau          #+#    #+#             */
-/*   Updated: 2020/11/19 16:36:22 by rberthau         ###   ########.fr       */
+/*   Updated: 2021/03/17 20:29:14 by roms             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	t_list *tmp;
 
 	tmp = *lst;
-	if (*lst != NULL && lst != NULL)
+	if (lst != NULL && *lst != NULL)
 	{
 		while (*lst != NULL)
 		{
